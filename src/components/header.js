fe00d7f56d36632765475d0498d883f4b7dd56ba
keeyan <keeyanhaghshenas9@gmail.com>
2021-12-13 13:49:47 -0500

    change logo to bring you to homepage

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,7 @@ const header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Keeyan Haghshenas</Navbar.Brand>
+        <Navbar.Brand href="/">Keeyan Haghshenas</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
